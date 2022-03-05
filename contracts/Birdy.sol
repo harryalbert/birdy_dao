7a082b168abda5ef7c946f8526907697dcff9935
harryalbert <harryalbert364@gmail.com>
2022-03-05 14:18:48 -0500

    created base contract extending ERC721

--- contracts/Birdy.sol
+++ contracts/Birdy.sol
@@ -1,18 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-import "@openzeppelin/contracts/utils/Counters.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 import "hardhat/console.sol";
 
-contract Birdy {
-    string private greeting;
+contract Birdy is ERC721{
+    string greeting = "";
 
-    constructor(string memory _greeting) {
-        console.log("Deploying a Greeter with greeting:", _greeting);
-        greeting = _greeting;
+    constructor() ERC721("Birdy", "BRDY") {
+        console.log("hello world");
     }
 
     function greet() public view returns (string memory) {
